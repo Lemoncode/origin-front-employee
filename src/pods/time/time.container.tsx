25b8bac7218727faf8dceb40bd1d3b3ff0d5e24f
Joel Kobjoll <kobjoll@gmail.com>
2020-09-20 21:59:09 +0200

    Pass states to TimeComponent

--- src/pods/time/time.container.tsx
+++ src/pods/time/time.container.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { TimeComponent } from './time.component';
-import { header } from './time.mock';
 import { useProjects } from './time.hooks';
 
 export const TimeContainer: React.FC = () => {
-  const { projects, updateProject, counters } = useProjects();
+  const {
+    counters,
+    disabled,
+    headers,
+    projects,
+    updateProject,
+    updateWeek,
+    week,
+  } = useProjects();
 
   return (
     <TimeComponent
+      columns={headers}
+      counters={counters}
+      disabled={disabled}
       projects={projects}
       updateProject={updateProject}
-      columns={header}
-      counters={counters}
+      updateWeek={updateWeek}
+      week={week}
     />
   );
 };
